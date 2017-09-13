0bbf894dac68fc667d987f0e73fec2715350d644
Deanna Lepke <deannalepke@gmail.com>
2017-09-13 03:19:32 +0000

    finish day 1 work for project #2

--- express_server.js
+++ express_server.js
@@ -2,18 +2,34 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
+function generateRandomString() {
+  var chars = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var length = 6;
+  var result = '';
+  for (var i = length; i > 0; i--) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+}
+
+//this approach might produce duplicates of existing strings in use,
+//so the same url -> different possible sources -> conflict
+
+//could check for existing urls then if the one generated is already in use,
+//go through and generate a new one for the same request
+
+//console.log(generateRandomString()); --> it works!!
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-  "4hfu43": "http://www.facebook.com"
+  "9sm5xK": "http://www.google.com"
 };
-
-// urlDatabase[generateRandomString()] =
 
 
 
@@ -30,8 +46,13 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase,
+                       longURL: urlDatabase[req.params.id] };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:id', (req, res) => {
@@ -40,37 +61,26 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
+
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  var randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+  res.redirect(`/urls/${randomString}`);
+  //res.send("Ok");
 });
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+
 });
 
 
-function generateRandomString() {
-  var chars = '1234567890abcdefghijklmnopqrstuvwxyz';
-  var length = 6;
-  var result = '';
-  for (var i = length; i > 0; i--) {
-    result += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return result;
-}
 
-//this approach might produce duplicates of existing strings in use,
-//so the same url -> different possible sources -> conflict
-
-//could check for existing urls then if the one generated is already in use,
-//go through and generate a new one for the same request
-
-//console.log(generateRandomString()); --> it works!!
 
 
